48308a240aac969681a571ba7bf39ab18297535d
hukovu <144943846+hukovu@users.noreply.github.com>
2023-09-30 11:34:17 -0300

    lista_01_Wudson

---
+++ lista_de_exercicio01.py
@@ -0,0 +1,42 @@
+# %%
+print('me chamo hugo e amo programar, essa é a resposta da primeira questão')
+
+# %%
+nome = input('digite seu nome:')
+
+print('bom dia, ' + nome + '!')
+
+
+# %%
+numero = float(input('digite um número'))
+
+resultado = (((numero + 1357) * 8) ** 2)
+
+print('o resultado do calculo é:', resultado)
+
+
+# %%
+nome_usuario = input('digite seu nome:')
+
+nome_usuario = nome_usuario.upper()
+
+print('Você está convidado para a nossa festa, ' + nome_usuario + '!')
+
+
+# %%
+resultado_soma = 5 + 3
+
+resultado_subtracao = 16 - 8
+
+resultado_multiplicação = 4 * 2
+
+resultado_divisão = 64 / 8
+
+print('resultado da adição:', resultado_soma)
+print('resultado da subtração:', resultado_subtracao)
+print('resultado da multiplicação', resultado_multiplicação)
+print('resultado da divisão:', resultado_divisão)
+# %%
+
+
+# %%
